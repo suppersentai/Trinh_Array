494363393e74c7a664e6cbdf61202abc135258a6
Trương Chung Toàn <chungtoan94@gmail.com>
2022-06-13 21:44:57 +0700

    Add project files.

---
+++ baitapvemang/ArrayDemo.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitapvemang
+{
+    public class ArrayDemo
+    {
+        public static void TestDemo()
+        {
+            do
+            {
+                Console.Write("Nhap bai ban muon chay demo: ");
+                string bai = Console.ReadLine();
+                if (bai == "1") bai1();
+                else if (bai == "2") bai2();
+                else if (bai == "3") bai3();
+            } while(true);
+            Console.ReadKey();  
+            static void bai1()
+            {
+                Console.WriteLine("Nhap va xuat cac gia tri ma gia tri tai do la gia tri am mang mot chieu cac so thuc!\n");
+                int n = InputNumber("Nhap so phan tu ban muon nhap: ");
+                float[] array = new float[n];
+                //nhap gia tri phan tu cho mang
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write("array [{0}]: ", i);
+                    array[i] = float.Parse(Console.ReadLine());
+                }
+                //xuat gia tri phan tu cho mang
+                Console.Write("Gia tri phan tu cua mang la: ");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write("-" + array[i] + " ");
+                }
+                Console.ReadKey();
+            }
+            static void bai2()
+            {
+                Console.WriteLine("Nhap va xuat cac gia tri chan trong mang mot chieu cac so nguyen!\n");
+                int n = InputNumber("Nhap so phan tu ban muon nhap: ");
+                int[] array = new int[n];
+                //nhap gia tri phan tu cho mang
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write("arry [{0}]: ", i);
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+                // xuat gia tri chan trong phan tu cua mang
+                Console.Write("Gia tri cac phan tu cua mang la: ");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] % 2 == 0)
+                    {
+                        Console.Write(array[i] + " ");
+                    }
+                    else Console.WriteLine("Khong co gia tri chan!");break;
+                }               
+                Console.ReadKey();
+            }
+            static void bai3()
+            {
+                Console.WriteLine("Tim gia tri lon nhat trong mang mot chieu cac so thuc!");
+                int n = InputNumber("Nhap vao so phan tu ban muon nhap: ");
+                float [] array = new float[n];
+                //nhap gia tri phan tu cho mang
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine("array [{0}]: ", i);
+                    array[i]=float.Parse(Console.ReadLine());
+                }
+                //xuat gia tri lon nhat trong mang
+                float max = array[0];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (max < array[i])
+                    {
+                        max = array[i];
+                           
+                    }
+                    Console.WriteLine("Gia tri lon nhat la {0} ",array[i]);
+                }
+                Console.ReadKey();
+            }    
+            static int InputNumber(string text)
+            {
+                int val = 0;
+                while (val == 0)
+                {
+                    Console.Write(text);
+                    var input = Console.ReadLine();
+                    int.TryParse(input.ToString(), out val);
+                    if (val != 0) return val;
+                }
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ baitapvemang/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace baitapvemang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            ArrayDemo.TestDemo();
+            Console.ReadKey();
+            /*  Console.WriteLine("Bai tap ve mang \n");
+              Console.WriteLine("Nhap so phan tu: ");
+              int[] n = new int[10];//n la mot mang bao gom 10 so nguyen 
+              int i, j;
+              //khoi tao cac phan tu cua mang n
+              for (i = 0; i < n.Length; i++)
+              {
+                  n[i] = i + 100;
+              }    
+              // hien thi cac gia tri phan tu cua mang
+              for (j = 0; j < n.Length; j++)
+              {
+                  Console.WriteLine("phan tu [{0}] = {1}", j, n[j]);
+              }   
+              Console.ReadKey();*/
+            /*Console.WriteLine("BAI TAP MANG 2 CHIEU!\n");
+            Console.WriteLine("Moi ban nhap so dong cua mang: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Moi ban nhap so cot cua mang: "); ;
+            int col = int.Parse(Console.ReadLine());
+            //tao mang 2 chieu voi so dong va so cot da nhap
+            int [,] IntArray = new int[row, col];
+            for (int i = 0; i < IntArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < IntArray.GetLength(1); j++)
+                {
+                    Console.WriteLine("Moi ban nhap phan tu IntArray [{0},{1}] = ", i, j);
+                    IntArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            int sum  = 0;
+            Console.WriteLine("\n Mang ban vua nhap la: ");
+            for (int i = 0; i < IntArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < IntArray.GetLength(1); j++)
+                {
+                    Console.Write(IntArray[i, j] + " ");
+                    sum += IntArray[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Tong cac gia tri trong mang: " + sum);
+            Console.ReadKey();*/
+            int row, col;
+            Console.Write("Nhap so dong: ");
+            row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so cot: ");
+            col = int.Parse(Console.ReadLine());
+            int[,] IntArray = new int[row, col];
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Random random = new Random();
+                    IntArray[i, j] = random.Next(IntArray[i, j]);
+                }
+            }
+            Console.WriteLine("Mang sau khi xuat ngay nhien la: ");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Random random = new Random();
+                    IntArray[i, j] = random.Next(IntArray[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
